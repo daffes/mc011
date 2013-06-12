936f980b60e3fbb9d06062ede646477addd4712a
Alexandre Nobuo Kunieda <alexandre.kunieda@gmail.com>
2013-06-12 15:58:31 -0300

    Dead Code Elimination is working!

--- lab3/src/optimization/DeadCodeElimination.java
+++ lab3/src/optimization/DeadCodeElimination.java
@@ -2,10 +2,12 @@ package optimization;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
 import temp.Temp;
 import util.List;
 import assem.Instr;
 import assem.OPER;
+import assem.MOVE;
 import flow_graph.AssemFlowGraph;
 import graph.Node;
 
@@ -15,48 +17,74 @@ public class DeadCodeElimination {
 	private LivenessAnalysis dfa;
 	private AssemFlowGraph cfg;
 
+	private Boolean compareListInstr(List<Instr> k, List<Instr> l) {
+	    Iterator kit = k.iterator();
+	    for (Instr lop : l) {
+		if (!kit.hasNext()) return false;
+		Instr kop = (Instr) kit.next();
+		if (!lop.debug().equals(kop.debug())) // kinda ugly -- replace?
+		    return false;
+	    }
+	    if (kit.hasNext()) return false;
+	    return true;
+	}
+
 	/*
 	 * realiza a otimizacao "dead code elimination" utilizando
 	 * a analise de fluxo de dados "liveness analysis"
 	 */
 	public List<Instr> optimize(List<Instr> l) {
-		cfg = new AssemFlowGraph(l);
-		/* executa a analise de longevidade */
-		dfa = new LivenessAnalysis(cfg);
+		int nopt = 0;
+		List<Instr> k = null;
 
-		// insira seu codigo aqui
-		List<Instr> k = new List<Instr>();
+		do {
 
-		for (Node n : dfa.flowGraph.nodes()) {
-		    if (dfa.flowGraph.instr(n) instanceof OPER) {
-			OPER op = (OPER) dfa.flowGraph.instr(n);
-			if (op.assem.startsWith("mov `d0, [") ||
-			    op.assem.startsWith("add") ||
-			    op.assem.startsWith("sub") ||
-			    op.assem.startsWith("imul") ||
-			    op.assem.startsWith("idiv") ||
-			    op.assem.startsWith("and") ||
-			    op.assem.startsWith("or") ||
-			    op.assem.startsWith("xor") ||
-			    op.assem.startsWith("shl") ||
-			    op.assem.startsWith("shr") ||
-			    op.assem.startsWith("sar")) {
-			    Set<Temp> opDef = new HashSet<Temp>();
-			    if (op.def() != null)
-				for (Temp t : op.def())
-				    opDef.add(t);
-			    opDef.retainAll(dfa.out.get(n));
-			    if (opDef.size() == 0) {
-				System.out.println("- " + dfa.flowGraph.instr(n).debug());
-				continue;
+		    if (k != null) l = k;
+		    k = new List<Instr>();
+
+		    cfg = new AssemFlowGraph(l);
+		    /* executa a analise de longevidade */
+		    dfa = new LivenessAnalysis(cfg);
+
+		    for (Node n : dfa.flowGraph.nodes()) {
+			if (dfa.flowGraph.instr(n) instanceof OPER ||
+			    dfa.flowGraph.instr(n) instanceof MOVE) {
+			    Instr op = dfa.flowGraph.instr(n);
+			    if (op.assem.startsWith("mov `d0,") ||
+				op.assem.startsWith("add") ||
+				op.assem.startsWith("sub") ||
+				op.assem.startsWith("imul") ||
+				op.assem.startsWith("idiv") ||
+				op.assem.startsWith("and") ||
+				op.assem.startsWith("or") ||
+				op.assem.startsWith("xor") ||
+				op.assem.startsWith("shl") ||
+				op.assem.startsWith("shr") ||
+				op.assem.startsWith("sar")) {
+				Set<Temp> opDef = new HashSet<Temp>();
+				if (op.def() != null)
+				    for (Temp t : op.def())
+					opDef.add(t);
+				opDef.retainAll(dfa.out.get(n));
+				if (opDef.size() == 0) {
+				    OPER x = new OPER("; " + op.assem,
+						      op.def(),
+						      op.use());
+				    k.append(x);
+				    // System.out.println("- " + op.debug());
+				    nopt++;
+				    continue;
+				}
 			    }
 			}
+
+			k.append(dfa.flowGraph.instr(n));
+			// System.out.println("+ " + dfa.flowGraph.instr(n).debug());
 		    }
 
-		    k.append(dfa.flowGraph.instr(n));
-		    // System.out.println("+ " + dfa.flowGraph.instr(n).debug());
-		}
+		} while (!compareListInstr(k, l));
 
+		System.out.println(nopt + " instructions less!");
 		return k;
 	}
 }
